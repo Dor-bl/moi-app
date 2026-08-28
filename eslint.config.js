bce979fb750533a8e093587db1e0f95dded343f2
Dor-bl <dblayzer@gmail.com>
2026-08-28 15:29:21 +0200

    feat: add ESLint static analysis and GitHub Actions CI workflow

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["app.js", "config.js", "config.template.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        L: "readonly",
+        supabase: "readonly",
+        CONFIG: "readonly",
+      },
+    },
+    rules: {
+      "no-undef": "error",
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    },
+  },
+  {
+    ignores: ["node_modules/"],
+  },
+];
